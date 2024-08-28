2a72bb1a41d39876edf87d6ee52f5b2c35157688
Maneesh Babu M <maneeshbabum@gmail.com>
2024-08-28 20:39:18 +0530

    feat: heroku integration

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
